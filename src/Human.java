3bbe9e3af9825f1cdf005858e02ce494a769a2c9
Рустам <rustam19871203@gmail.com>
2022-11-07 14:10:06 +0400

    ООП Домашка 1

--- src/Human.java
+++ src/Human.java
@@ -3,22 +3,13 @@ public class Human {
     public String town;
     public int yearOfBirth;
     public String jobTitle;
-
-    public Human(String name, String town, int yearOfBirth, String jobTitle) {
-        this.name = name;
-        this.town = town;
-        this.yearOfBirth = yearOfBirth;
-        this.jobTitle = jobTitle;
-    }
-
     void info() {
-        System.out.println("Привет! Меня зовут " + name + ". Я из города " + town + ". Я родился(-ась)" + yearOfBirth + " году. Я работаю на должности " + jobTitle + ". Будем знакомы!");
+        System.out.println("Привет! Меня зовут " + name + ". Я из города " + town + ". Я родился(-ась) " + yearOfBirth + " году. Я работаю на должности " + jobTitle + ". Будем знакомы!");
         System.out.println("--------------------------------------------------------------------------------------------------------------------------------------------------------------");
     }
-
-    //-------------С методом toString---------------
-    /*@Override
-    public String toString() {
-        return "Привет! Меня зовут " + name + ". Я из города " + town + ". Я родился(-ась)" + yearOfBirth + " году. Я работаю на должности " + jobTitle + ". Будем знакомы!";
-    }*/
 }
+
+
+
+
+
--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        Human maxim = new Human("Максим", "Минск", 1988, "бренд-менеджером");
-        Human anna = new Human("Аня", "Москва", 1993, "методистом образовательных программ");
-        Human kate = new Human("Катя", "Калининград", 1992, "продакт-менеджером");
-        Human artem = new Human("Артем", "Москва", 1995, "директором по развитию бизнеса");
 
+        Human maxim = new Human();
+        maxim.name = "Максим"; maxim.town = "Минск"; maxim.yearOfBirth = 1988; maxim.jobTitle = "бренд-менеджер";
+        Human anna = new Human();
+        anna.name = "Аня"; anna.town = "Москва"; anna.yearOfBirth = 1993; anna.jobTitle =  "методист образовательных программ";
+        Human kate = new Human();
+        kate.name = "Катя"; kate.town = "Калининград"; kate.yearOfBirth = 1992; kate.jobTitle = "продукт-менеджер";
+        Human artem = new Human();
+        artem.name = "Артем"; artem.town = "Москва"; artem.yearOfBirth = 1995; artem.jobTitle = "директор по развитию бизнеса";
         maxim.info();
         anna.info();
         kate.info();
         artem.info();
-
-        //--------------С методом toString---------------------
-        /*System.out.println(maxim.toString());
-        System.out.println(anna.toString());
-        System.out.println(kate.toString());
-        System.out.println(artem.toString());
-*/
-
-
     }
 }
+
+
+
+
+
