07de0769f8f0899199c55ae4c7cbf961b55cedc4
Рустам <rustam19871203@gmail.com>
2022-11-07 12:27:54 +0400

    ООП Домашка 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
+        Human maxim = new Human("Максим", "Минск", 1988, "бренд-менеджером");
+        Human anna = new Human("Аня", "Москва", 1993, "методистом образовательных программ");
+        Human kate = new Human("Катя", "Калининград", 1992, "продакт-менеджером");
+        Human artem = new Human("Артем", "Москва", 1995, "директором по развитию бизнеса");
+
+        maxim.info();
+        anna.info();
+        kate.info();
+        artem.info();
+
+        //--------------С методом toString---------------------
+        /*System.out.println(maxim.toString());
+        System.out.println(anna.toString());
+        System.out.println(kate.toString());
+        System.out.println(artem.toString());
+*/
+
 
     }
 }
